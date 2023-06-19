cdaf2914f77f1edd4bf5a423c1a1e5577cf832e6
SurfingOwl <arthur.cesar.fr@gmail.com>
2023-06-19 19:02:53 +0200

    run

--- Program.cs
+++ Program.cs
@@ -3,6 +3,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Vehicules vehicules = new Vehicules();
+        vehicules.Attach(new Car("Voiture", 0, 100, 10));
+        vehicules.Attach(new Bike("Moto", 0, 100, 5));
+
+        MileageVisitor mileageVisitor = new MileageVisitor();
+        FuelComsuptionVisitor fuelComsuptionVisitor = new FuelComsuptionVisitor();
+
+        vehicules.Accept(mileageVisitor);
+        vehicules.Accept(fuelComsuptionVisitor);
     }
 }
